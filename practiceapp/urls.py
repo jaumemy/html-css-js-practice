a2c03da8522fd822ec06638b9b8ff29e70dc1d11
jaumemy <jaume.montane@outlook.com>
2020-12-09 22:29:18 -0500

    5 - nice login

--- practiceapp/urls.py
+++ practiceapp/urls.py
@@ -7,4 +7,5 @@ app_name = "practiceapp"
 urlpatterns = [
     path('', views.index, name='index'),
     path('blog_cards/', views.blog_cards, name="blog_cards"),
+    path('nice_login/', views.nice_login, name="nice_login"),
 ]
--- practiceapp/views.py
+++ practiceapp/views.py
@@ -7,3 +7,6 @@ def index (request):
 
 def blog_cards(request):
     return render(request,"practiceapp/blog_cards.html")
+
+def nice_login(request):
+    return render(request,"practiceapp/nice_login.html")
